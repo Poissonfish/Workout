2b513c7602cfa8c231ea5c305fd8cb22feb2bbba
Poissonfish <b99612021@ntu.edu.tw>
2017-04-22 22:44:59 -0700

    Apr 22, 17 - plot layout adjustment

--- src/workout.r
+++ src/workout.r
@@ -10,12 +10,11 @@ data[, Date := data$Date %>% as.Date]
 data[, Month := month(data$Date, label = T)]
 data$Body[data$Exercise%in%c('Bench Press', 'Pec Deck')] = 'Upper body'
 data$Body[data$Exercise%in%c('Deadlift', 'Squat')] = 'Lower body'
-data$Body = data$Body  %>% factor(levels = c('Upper body', 'Lower body'))
+data$Body = data$Body %>% factor(levels = c('Upper body', 'Lower body'))
 
-pos = position_dodge(width=(.2))
 plot = ggplot(data, aes(x = Date, y = Weight, colour = Exercise, size = Replicate, group = ID)) + 
-geom_point(alpha=.5, position=pos) +
-scale_x_continuous(limit = c(min(data$Date)-2, max(data$Date)+2))+
-scale_y_continuous(limit = c(50, 250), breaks= seq(50, 250, 20))+
-facet_grid(Body~Month)
+	geom_point(alpha = .5, position = position_dodge(width = (.7))) +
+	scale_x_date(limits = c(min(data$Date) - 2, max(data$Date) + 2)) +
+	scale_y_continuous(limits = c(50, 250), breaks = seq(50, 250, 20)) +
+	facet_grid(Body ~ Month)
 ggsave(filename = '../plot.png', plot = plot) 
