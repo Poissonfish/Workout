f7a24e5f1e49b16e11d43491785c25b2322bf115
Poissonfish <b99612021@ntu.edu.tw>
2017-04-23 12:04:27 -0700

    Add comments

--- src/workout.r
+++ src/workout.r
@@ -1,20 +1,34 @@
+# Import libraries
 library(data.table)
 library(magrittr)
 library(lubridate)
 library(ggplot2)
 setwd('~/git/Workout/data/')
-data = fread('data.txt', colClasses = c(Date = 'character', Exercise = 'factor', Replicate = 'numeric', Weight = 'numeric'), blank.lines.skip = TRUE)
+
+# Define variables
+Upper_Exercise = c('Bench Press', 'Pec Deck')
+Lower_Exercise = c('Deadlift', 'Squat')
+
+# Import data
+data = fread('data.txt', blank.lines.skip = TRUE,
+			 colClasses = c(Date = 'character', 
+							Exercise = 'factor', 
+							Replicate = 'numeric', 
+							Weight = 'numeric'))
+
+# Construct the data table
 data[, Uni := paste0(Date, Exercise)]
 data[, ID := factor(rowid(Uni))]
 data[, Date := data$Date %>% as.Date]
-data[, Month := month(data$Date, label = T)]
-data$Body[data$Exercise%in%c('Bench Press', 'Pec Deck')] = 'Upper body'
-data$Body[data$Exercise%in%c('Deadlift', 'Squat')] = 'Lower body'
+data$Body[data$Exercise %in% Upper_Exercise] = 'Upper body'
+data$Body[data$Exercise %in% Lower_Exercise] = 'Lower body'
 data$Body = data$Body %>% factor(levels = c('Upper body', 'Lower body'))
 
+# Plotting
 plot = ggplot(data, aes(x = Date, y = Weight, colour = Exercise, size = Replicate, group = ID)) + 
+	geom_hline(yintercept = 135, colour = 'red', alpha = .5) +
 	geom_point(alpha = .5, position = position_dodge(width = (.7))) +
 	scale_x_date(limits = c(min(data$Date) - 2, max(data$Date) + 2)) +
-	scale_y_continuous(limits = c(50, 250), breaks = seq(50, 250, 20)) +
-	facet_grid(Body ~ Month)
+	scale_y_continuous(name = 'Weight (lb)', limits = c(50, 250), breaks = seq(50, 250, 20)) +
+	theme(axis.title.x = element_blank()) + facet_grid(Body ~ .)
 ggsave(filename = '../plot.png', plot = plot) 
