908acad04b0bc40180279d9f1c3d3d2e52adf9ae
Poissonfish <b99612021@ntu.edu.tw>
2017-05-02 16:49:40 -0700

    Update data

--- src/workout.r
+++ src/workout.r
@@ -27,7 +27,7 @@ data$Body = data$Body %>% factor(levels = c('Upper body', 'Lower body'))
 # Plotting
 plot = ggplot(data, aes(x = Date, y = Weight, colour = Exercise, size = Replicate, group = ID)) + 
 	geom_hline(yintercept = 135, colour = 'red', alpha = .5) +
-	geom_point(alpha = .5, position = position_dodge(width = (.7))) +
+	geom_point(alpha = .5, position = position_dodge(width = (1.7))) +
 	scale_x_date(limits = c(min(data$Date) - 2, max(data$Date) + 2)) +
 	scale_y_continuous(name = 'Weight (lb)', limits = c(50, 250), breaks = seq(50, 250, 20)) +
 	theme(axis.title.x = element_blank()) + facet_grid(Body ~ .)
